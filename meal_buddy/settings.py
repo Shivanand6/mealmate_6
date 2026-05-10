5f8702c7bd1031351b5569bd5ea7982da15281ce
Shivanand6 <shivanandshirur99@gmail.com>
2026-05-11 00:22:18 +0530

    Fixed static files

--- meal_buddy/settings.py
+++ meal_buddy/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -161,7 +162,17 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 
+import os
 
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'delivery/static')
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
